88a428a044abbbfef03998735d0c76e917295e40
chayanid Kaewla <ckaewla2@illinois.edu>
2022-04-07 17:11:18 -0500

    get list of financial firms from job role from H1B database implemented

--- WebScrape/webScrapingH1B.py
+++ WebScrape/webScrapingH1B.py
@@ -52,9 +52,10 @@ def load_data(str):
     df['month'] = df['submit date'].dt.month
     return df
 """
-load_com
-This functions takes in job roles and gets all the companies that have the input job roles.
-The list of the companies found are stored in the "company_set" set
+load_comp
+This functions takes in company name and gets all the information of that firm job title, base salary,
+location.
+From this information, we obtain we collect the superset of job role of HFT firms.
 """
 def load_company(str):
     word = str.split()
@@ -70,45 +71,52 @@ def load_company(str):
     soup = BeautifulSoup(r, features="html.parser")
     data2 = soup.find_all('tr')    
     labels = []
-    for h in data2[0].find_all('th'):
-        labels.append(h.get_text().strip().lower())
+    if(len(data2)>0):
+        for h in data2[0].find_all('th'):
+            try:
+                labels.append(h.get_text().strip().lower())
+            except:
+                print("skip one")
     
-    dict = {}
+        dict = {}
 
-    final = []
-    for data in data2[1:]:
-        data_list = []
-        for d in data.find_all('td'):
-            d_str = d.get_text().replace(',','')
+        final = []
+        for data in data2[1:]:
+            data_list = []
+            for d in data.find_all('td'):
+                d_str = d.get_text().replace(',','')
+            
+                if d_str.isnumeric():
+                    data_list.append(int(d_str))
+                else:
+                    data_list.append(d_str)                 
+            final.append(data_list)
+            
+            if len(data_list) >= 2:
+                job_superset.add(data_list[1])
+                if data_list[1] in dict:
+                    dict[data_list[1]] += 1
+                else:
+                    dict[data_list[1]] = 1
         
-            if d_str.isnumeric():
-                data_list.append(int(d_str))
-            else:
-                data_list.append(d_str)                 
-        final.append(data_list)
-        
-        if len(data_list) >= 2:
-            if data_list[1] in dict:
-                dict[data_list[1]] += 1
-            else:
-                dict[data_list[1]] = 1
-     
-    with open('jobrolefreqency.csv', 'a', encoding='UTF8') as f: 
-        writer = csv.writer(f)
-        for key in dict:
-            data = [str, key, dict[key]]
-            writer.writerow(data)
-    df = pd.DataFrame(final, columns = labels)    
-    df['submit date'] = pd.to_datetime(df['submit date'])
-    df['start date'] = pd.to_datetime(df['start date'])
-    df['state'] = df['location'].str.split().str[-1] 
-    df['year'] = df['submit date'].dt.year
-    df['month'] = df['submit date'].dt.month
-    return df
+            with open('jobrolefreqency.csv', 'a', encoding='UTF8') as f: 
+                writer = csv.writer(f)
+                for key in dict:
+                    data = [str, key, dict[key]]
+                    writer.writerow(data)
+            df = pd.DataFrame(final, columns = labels)    
+            df['submit date'] = pd.to_datetime(df['submit date'])
+            df['start date'] = pd.to_datetime(df['start date'])
+            df['state'] = df['location'].str.split().str[-1] 
+            df['year'] = df['submit date'].dt.year
+            df['month'] = df['submit date'].dt.month
+            return df
+    return None
 
 #example of how to run 
-job_list = ["QUANT", "TRADER", "ALGORITHMIC TRADER"]
+job_list = ["QUANT", "TRADER", "ALGORITHMIC TRADER", "PORTFOLIO"]
 company_set = set()
+job_superset = set()
 for job in job_list:
     load_data(job)
 print(company_set)
@@ -118,6 +126,8 @@ with open('jobrolefreqency.csv', 'a', encoding='UTF8') as f:
     writer = csv.writer(f)
     writer.writerow(header)
 for company in company_set:
-    load_company(company)
+    if company!= "PANAGORA ASSET MANAGEMENT INC":
+        load_company(company)
 print("finished writing to csv file")
+#print("\n"+str(len(job_superset)))
 
