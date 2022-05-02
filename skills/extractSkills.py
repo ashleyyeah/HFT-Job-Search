07f4836989bf583dcf43b7388e00839e5b4321e1
chayanid Kaewla <ckaewla2@illinois.edu>
2022-05-02 02:09:54 -0500

    correct role_skill table, update skills.csv to avoid wrong substring search

--- skills/extractSkills.py
+++ skills/extractSkills.py
@@ -7,38 +7,41 @@ def extractSkills():
     skill_dict = {}
     skill_found = list()
     id = 0
-    with open('skills/company_role_skill.csv','w', newline="\n")  as skill_file:
+    with open('company_role_skill/company_role_skill.csv','w', newline="\n")  as skill_file:
         writer = csv.writer(skill_file)
         to_write = ["COMPANY_ROLE_SKILLS_ID", "COMPANY_ROLES_ID","SKILL_ID"]
         writer.writerow(to_write)
     #intialize the dict with role (value) and its id (key)
-    with open('skills/roles.csv') as csv_file:
+    with open('roles/roles.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if row[0] != "ROLE_ID":
                 role_dict[row[0].upper()] = row[1]
+
     with open('skills/skills.csv', newline='') as f:
         csv_reader = csv.reader(f)
         for row in csv_reader:
             skill = row[1]
-            print(skill)
+            #print(skill)
             if skill != '' and skill != 'SKILL':
                 skill_dict[skill.upper()] = row[0]
-        print(skill_dict)
-    with open('skills/company_role_specs_updated.csv') as csv_file:
+        #print(skill_dict)
+    with open('roles/company_roles.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
-                if row[0] != "COMPANY_ROLES_SPECS_ID":
+                if row[0] != "COMPANY_ROLES_ID":
                     try:
-                        role_name = role_dict[row[1]].upper()
+                        role_id = row[2]
+                        role_name = role_dict[role_id]
                     except:
-                        not_found.append(row[1])
+                        not_found.append(row[0])
+                        continue
                     for skill in skill_dict:
                         if(role_name.find(skill.upper()) != -1):
                             
-                            with open('skills/company_role_skill.csv','a', newline="\n")  as skill_file:
+                            with open('company_role_skill/company_role_skill.csv','a', newline="\n")  as skill_file:
                                 writer = csv.writer(skill_file)
-                                to_write = [id, row[1],skill_dict[skill]]
+                                to_write = [id, row[0],skill_dict[skill]]
                                 writer.writerow(to_write)
                                 id=id+1
                             if skill not in skill_found:
