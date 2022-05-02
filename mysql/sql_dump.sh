35d20cf43632ac9fdfe01cda73f2982dc70ea217
Ashley Yeah <ayeah2@illinois.edu>
2022-05-02 16:58:37 +0000

    missed some files for new provision

--- mysql/sql_dump.sh
+++ mysql/sql_dump.sh
@@ -4,8 +4,10 @@ mysql -u root -pvagrant <<_EOF_
 CREATE database project;
 _EOF_
 
-mysql -u root -pvagrant < Dump20220414/project_routines.sql
-mysql -u root -pvagrant project < Dump20220414/project_companies.sql
+mysql -u root -pvagrant < Dump20220502/project_routines.sql
+mysql -u root -pvagrant project < Dump20220502/project_companies.sql
+mysql -u root -pvagrant project < Dump20220502/project_roles.sql
+mysql -u root -pvagrant project < Dump20220502/project_company_roles.sql
 
 echo "Finished creating MySQL schema and table"
 
