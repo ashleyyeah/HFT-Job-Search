6c4e685d827ad0fd5775ab3fb173c348c414df90
Ashley Yeah <ayeah2@illinois.edu>
2022-05-03 22:09:05 +0000

    Add table to sql bash script

--- mysql/sql_dump.sh
+++ mysql/sql_dump.sh
@@ -8,6 +8,7 @@ mysql -u root -pvagrant < Dump20220502/project_routines.sql
 mysql -u root -pvagrant project < Dump20220502/project_companies.sql
 mysql -u root -pvagrant project < Dump20220502/project_roles.sql
 mysql -u root -pvagrant project < Dump20220502/project_company_roles.sql
+mysql -u root -pvagrant project < Dump20220502/project_company_role_specs.sql
 
 echo "Finished creating MySQL schema and table"
 
