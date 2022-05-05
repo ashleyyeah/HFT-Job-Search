61f71ff5ca5c63360836de383da2930731120122
Brennan Eng <bheng2@illinois.edu>
2022-05-05 06:33:06 +0000

    Added more UI design to project such as Autocomplete textfields

--- interface/src/Game.js
+++ interface/src/Game.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 //import Select from '@types/react-select'
 import './index.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { Button } from '@mui/material'
+import { Button, Autocomplete, TextField, Stack } from '@mui/material';
 
 const data = [
     {
@@ -35,6 +35,8 @@ const data = [
     }
 ];
 
+// const hftfirms = ['citadel', 'jumptrading', 'jpmorgan'];
+
 export const Board = (props) => {
     const [value, setValue] = useState('');
 
@@ -57,27 +59,53 @@ export const Board = (props) => {
     );
 }
 
+export const HFTAutocomplete = () => {
+    const [selectedHFTfirm, setselectedHFTfirm] = useState(null);
+    return (
+        <Autocomplete
+            id="HFT Firms"
+            options={data}
+            sx = {{width:300}}
+            renderInput={(params) => (<TextField {...params} label = "Choose HFT Firm here"
+            placeholder = "Arbitrary HFT Firm" />)}
+            getOptionLabel={option => option.name}
+            value = {selectedHFTfirm}
+            onChange={(_event, newHFTfirm) => {
+                setselectedHFTfirm(newHFTfirm);
+            }}
+        />
+    )
+}
+
+export const HFTJobAutocomplete = () => {
+    const [selectedHFTfirm, setselectedHFTfirm] = useState(null);
+    return (
+        <Autocomplete
+            id="HFT Firms"
+            options={data}
+            sx = {{width:300}}
+            renderInput={(params) => (<TextField {...params} label = "Choose HFT Job Position here"
+            placeholder = "Arbitrary HFT Position" />)}
+            getOptionLabel={option => option.name}
+            value = {selectedHFTfirm}
+            onChange={(_event, newHFTfirm) => {
+                setselectedHFTfirm(newHFTfirm);
+            }}
+        />
+    )
+}
+
 export const Game = (props) => {
     return (
         <div className="game">
-            <div width="50%" height="100%">
-                <div className="game-board">
-                    <h1> HFT Job Database </h1>
-                    <h4> created by: Brennan Eng, Ashley Yeah, Jeep Kaewla, Sanjana Pingali </h4>
-                    Input HFT firm: <Board />
-                    <div></div>
-                    Positions available: <Board />
-                    <div />
-                    <h3> Salary Range </h3>
-                    From: <Board />
-                    <div></div>
-                    To: <Board />
-                    <div />
-                    Skills: <Board />
-                    Location: <Board />
-                </div>
-                <Button>BING</Button>
-            </div>
+            <Stack spacing={2}>
+                <h1>HFT Job Industry Database</h1>
+                <h3> designed by: Brennan Eng, Ashley Yeah, Jeep Kaewala, and Sanjana Pingali</h3>
+                <h3>Input Values here:</h3>
+                <HFTAutocomplete />
+                <HFTJobAutocomplete />
+                <Button variant='contained' sx= {{width:100}}>Submit</Button>
+            </Stack>
             <ResponsiveContainer width="50%" aspect={1}>
                 <LineChart
                     width={500}
