5069f229867b86e9686794b5e34197e4e83cea5c
Ashley Yeah <ayeah2@illinois.edu>
2022-05-07 17:29:59 +0000

    Fix typo in sql script

--- mysql/sql_dump.sh
+++ mysql/sql_dump.sh
@@ -9,7 +9,7 @@ mysql -u root -pvagrant project < FinalDump/project_companies.sql
 mysql -u root -pvagrant project < FinalDump/project_roles.sql
 mysql -u root -pvagrant project < FinalDump/project_company_roles.sql
 mysql -u root -pvagrant project < FinalDump/project_company_role_specs.sql
-mysql -u root -pvagrant project < FinalDump/project_skill.sql
+mysql -u root -pvagrant project < FinalDump/project_skills.sql
 mysql -u root -pvagrant project < FinalDump/project_company_role_skills.sql
 
 echo "Finished creating MySQL schema and table"
