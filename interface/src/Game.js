438e6421837454a1ecc4f27b40ced1e529be4644
pingali4 <pingali4@illinois.edu>
2022-05-15 23:10:16 +0000

    check h1

--- interface/src/Game.js
+++ interface/src/Game.js
@@ -701,6 +701,16 @@ export const Game = (props) => {
 };
 
 // testing
-var component = TestUtils.renderIntoDocument(
-  <MyComponent />
-);
+it("renders an h1", function () {
+  var component = TestUtils.renderIntoDocument(
+      <MyComponent />
+  );
+
+  var h1 = TestUtils.findRenderedDOMComponentWithTag(
+     component, 'h1'
+  );
+
+  expect(h1.getDOMNode().textContent)
+      .toEqual("A title");
+});
+
