5fc6b82cd1289fd848f17bb5494fb3986620d175
pingali4 <pingali4@illinois.edu>
2022-05-16 11:36:03 +0000

    updates versions

--- interface/src/__tests__/Game.test..js
+++ interface/src/__tests__/Game.test..js
@@ -2,8 +2,8 @@ import {render, screen, cleanup} from '@testing-library/react'
 import HFTSkillAutocomplete1 from '../Game';
 
 test('test for ', () =>{
-   // expect(true).toBe(true) ;
+   //expect(true).toBe(true) ;
    render(<HFTSkillAutocomplete1/>);
-   const check_elem = screen.getByTestId('Skill1');
+   const check_elem = screen.getByTestId('Skill-1');
    expect(check_elem).toBeInTheDocument();
 });
